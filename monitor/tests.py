087f816d4d71881af6c728de848a81e8326630be
Arpit Blagan <blaganarpit@gmail.com>
2025-01-18 12:44:37 +0530

    some updates

--- monitor/tests.py
+++ monitor/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
-from .models import Website
-from django.urls import reverse
+
+from monitor.views import notify_on_discord
+from .models import Website,StatusHistory
+# from django.urls import reverse
+from unittest.mock import patch, AsyncMock
 # Create your tests here.
 class TestModles(TestCase):
     def test_website(self):
@@ -9,5 +12,20 @@ class TestModles(TestCase):
 class TestViews(TestCase):
     def test_website_and_check(self):
         response=Website.objects.create(name="Demo2",url="http://localhost:3000")
-        print("response",response)
         self.assertEqual(Website.objects.last().name, 'Demo2')
+    
+    @patch('httpx.AsyncClient.post')  # Mock the HTTP POST request
+    async def test_notify_on_discord_success(self, mock_post):
+        # Set up mock response
+        mock_post.return_value = AsyncMock(status_code=204)
+
+        # Create test data
+        website = Website(name="Test Website", url="http://example.com")
+        status_history = StatusHistory(status="up")
+
+        # Call the function
+        result = await notify_on_discord(website, status_history)
+        self.assertEqual(result,True)
+        # Assertions
+        self.assertTrue(result)
+        mock_post.assert_called_once() 
